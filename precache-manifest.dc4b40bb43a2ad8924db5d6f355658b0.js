c5b5dba326d2ed0186795b738266910031c6ada9
developersway <aman.ak2012@gmail.com>
2019-10-25 21:39:38 +0530

    Updates

---
+++ precache-manifest.dc4b40bb43a2ad8924db5d6f355658b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5176d23e69b23608fe9bebab90fdd6d5",
+    "url": "/tic_tac_toe/index.html"
+  },
+  {
+    "revision": "7b3e394034a3dc584e61",
+    "url": "/tic_tac_toe/static/css/main.86421cb8.chunk.css"
+  },
+  {
+    "revision": "0d93e0f65bdb366f6955",
+    "url": "/tic_tac_toe/static/js/2.a86ffbc3.chunk.js"
+  },
+  {
+    "revision": "7b3e394034a3dc584e61",
+    "url": "/tic_tac_toe/static/js/main.aa6d5cc1.chunk.js"
+  },
+  {
+    "revision": "2830918270d5941af995",
+    "url": "/tic_tac_toe/static/js/runtime-main.1263cce8.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/tic_tac_toe/static/media/logo.25bf045c.svg"
+  }
+]);
